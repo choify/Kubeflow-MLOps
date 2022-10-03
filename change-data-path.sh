a9e1320e46e37e58299b6530824963d31dc45ec1
Ubuntu <ubuntu@ip-172-31-44-143.ap-northeast-2.compute.internal>
2022-10-03 12:40:50 +0000

    add change-data-path.sh

--- change-data-path.sh
+++ change-data-path.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cat ./data/titanic.csv.dvc | grep md5 | cut -d ' ' -f3 | cut -c -2 > prefix1.txt
 cat ./data/titanic.csv.dvc | grep md5 | cut -d ' ' -f3 | cut -c 3- > prefix2.txt
-sed "16c\\object_key = 'data/$(cat prefix1.txt)/$(cat prefix2.txt)'" train.py
+sed -i "16c\\object_key = 'data/$(cat prefix1.txt)/$(cat prefix2.txt)'" train.py
 rm ./prefix1.txt
 rm ./prefix2.txt
